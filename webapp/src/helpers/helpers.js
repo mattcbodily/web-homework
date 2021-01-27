bbcd4f0a34601b80637f328db9212e37c91116c1
mattcbodily <mattcbodily@gmail.com>
2021-01-27 11:19:46 -0700

    romanize function moved to helpers file to be used universally

--- webapp/src/helpers/helpers.js
+++ webapp/src/helpers/helpers.js
@@ -22,3 +22,19 @@ export function validateUploadData (data) {
 
   return valid
 }
+
+export function romanize (num) {
+  if (num) {
+    const lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+    let roman = ''
+    let i
+    for (i in lookup) {
+      while (num >= lookup[i]) {
+        roman += i
+        num -= lookup[i]
+      }
+    }
+
+    return roman
+  }
+}
